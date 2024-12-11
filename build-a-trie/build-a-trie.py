42c60fdc40a845037814a550798e6fd57f77ff1c
JMarts32 <jm2002.santosa@hotmail.com>
2024-12-11 14:49:58 -0500

    Solution

--- build-a-trie/build-a-trie.py
+++ build-a-trie/build-a-trie.py
@@ -1,2 +1,26 @@
 def build_trie(*words):
-    return "Implement me! :)"
+    trie_dict = {}
+
+    for word in words:
+        current_dict = trie_dict
+        prev_word = ''
+        if word == '' and len(words) == 1:
+            return {}
+        for char in word:
+            current_word = prev_word + char
+            prev_word = current_word
+            if current_word not in current_dict:
+                current_dict[current_word] = {}
+            current_dict = current_dict[current_word]
+
+    def replace_empty_dicts(trie_dictionary):
+        for key, value in trie_dictionary.items():
+            if value == {}:
+                trie_dictionary[key] = None
+            elif isinstance(value, dict):
+                replace_empty_dicts(value)
+
+    replace_empty_dicts(trie_dict)
+    return trie_dict
+
+print(build_trie("A", "to", "tea", "ted", "ten", "i", "in", "inn"))
